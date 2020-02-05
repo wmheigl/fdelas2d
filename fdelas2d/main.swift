91756c385fb34de345b28126ee8bdcf6939dfe04
Werner M. Heigl <werner.heigl@me.com>
2020-02-05 12:26:14 -0700

    Initial Commit

---
+++ fdelas2d/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  fdelas2d
+//
+//  Created by Werner Heigl on 2/5/20.
+//  Copyright © 2020 Werner Heigl. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
